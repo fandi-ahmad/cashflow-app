4db9f938d54fb65dff95cbc0ca81d7859cc90e99
fandi-ahmad <fandi4160@gmail.com>
2024-08-03 22:13:30 +0800

    add function part

---
+++ function/index.ts
@@ -0,0 +1,5 @@
+import { cashFormated } from "./cashFormated";
+import { generateUniqueId } from "./generateUniqueId";
+import { getCurrentDateTime } from "./getCurrentDateTime";
+
+export { cashFormated, generateUniqueId, getCurrentDateTime }
