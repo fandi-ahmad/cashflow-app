e6b24be1ac00974e6192d26cbbdd905c335a2019
fandi-ahmad <fandi4160@gmail.com>
2024-08-06 15:31:29 +0800

    add more state

--- hooks/useGlobalState.ts
+++ hooks/useGlobalState.ts
@@ -4,12 +4,18 @@ type stateType = {
   totalCash: number,
   totalCashFormated: string,
   allDataCash: any,
+  isModalFormVisible: boolean,
+  cashActionText: string,
+  typeCash: 'income' | 'spending',
 }
 
 const initialState = <stateType> {
   totalCash: NaN,
   totalCashFormated: '',
   allDataCash: [],
+  isModalFormVisible: false,
+  cashActionText: '',
+  typeCash: 'income',
 };
 const { useGlobalState } = createGlobalState(initialState);
 
