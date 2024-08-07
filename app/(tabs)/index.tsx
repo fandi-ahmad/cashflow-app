3bf83c7ee9e4055e68007fcc141d4357769c8767
fandi-ahmad <fandi4160@gmail.com>
2024-08-07 22:34:24 +0800

    add filter for month

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet, View, Text, ScrollView, Pressable } from 'react-native';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HeroSection } from '@/components/HeroSection';
 import { useGlobalState } from '@/hooks/useGlobalState';
 import CardList from '@/components/home/CardList';
-import CheckBox from '@/components/CheckBox';
 import ModalForm from '@/components/home/ModalForm';
-import AsyncStorage from "@react-native-async-storage/async-storage"
 import { cashFormated } from '@/function';
+import Modal from "react-native-modal"
+import MonthOption from '@/components/home/MonthOption';
+import { getAllDataCashByMonth, getAllDataCashByType, getAllDataCashByMonthType, getAllDataCash } from '@/function/dataCash';
 
 export default function HomeScreen() {
   const [isModalFormVisible, setIsModalFormVisible] = useGlobalState('isModalFormVisible')
@@ -15,12 +16,14 @@ export default function HomeScreen() {
   const [allDataCash, setAllDataCash] = useGlobalState('allDataCash')
   const [typeCash, setTypeCash] = useGlobalState('typeCash')
 
-  const [isFilterIncome, setIsFilterIncome] = useState(false)
-  const [isFilterSpending, setIsFilterSpending] = useState(false)
+  // button active style
+  const [isFilterIncome, setIsFilterIncome] = useGlobalState('isFilterIncome')
+  const [isFilterSpending, setIsFilterSpending] = useGlobalState('isFilterSpending')
 
   // filter
-  const [totalCashFilter, setTotalCashFilter] = useState<number>()
+  const [totalCashFilter, setTotalCashFilter] = useGlobalState('totalCashFilter')
   const [isShowFilterContainer, setIsShowFilterContainer] = useState<boolean>(false)
+  const [typeCashFilter, setTypeCashFilter] = useGlobalState('typeCashFilter')
 
   const showModalandSetType = (actionType = '', typeCash: 'income' | 'spending') => {
     setCashActionText(actionType)
@@ -37,31 +40,74 @@ export default function HomeScreen() {
       setIsFilterIncome(!isFilterIncome)
       isFilterSpending ? setIsFilterSpending(false) : null
       isFilterIncome ? getAllDataCashForFilter('') : getAllDataCashForFilter('income')
+      isFilterIncome ? setTypeCashFilter('') : setTypeCashFilter(filterType)
     }
 
     if (filterType === 'spending') {
       setIsFilterSpending(!isFilterSpending)
       isFilterIncome ? setIsFilterIncome(false) : null
       isFilterSpending ? getAllDataCashForFilter('') : getAllDataCashForFilter('spending')
+      isFilterSpending ? setTypeCashFilter('') : setTypeCashFilter(filterType)
     }
 
   }
   
   
   const getAllDataCashForFilter = async (filterType: 'income' | 'spending' | '') => {
-    const value = await AsyncStorage.getItem('cash');
-
-    if (value) {
-      const jsonValue = JSON.parse(value);
-
-      const filteredData = filterType ? jsonValue.filter((item: any) => item.type === filterType) : jsonValue;
-      setAllDataCash(filteredData);
-
-      const total = filterType ? filteredData.reduce((acc: number, item: any) => acc + item.amount, 0) : 0;
-      setTotalCashFilter(total);
+    if (month) {
+      getDataByMonth()
+    } else {
+      const data = await getAllDataCashByType(filterType)
+      setAllDataCash(data?.data)
+      setTotalCashFilter(data?.totalCashFilter)
     }
   }
 
+
+  const [isModalMonthVisible, setIsModalMonthVisible] = useState<boolean>(false)
+  const [year, setYear] = useGlobalState('yearFilter')
+  const [month, setMonth] = useGlobalState('monthFilter')  /* <-- in number */
+
+  const getCurrentYear = () => {
+    const currentYear = new Date().getFullYear()
+    setYear(currentYear)
+  };
+
+  const showModalMonth = () => {
+    setIsModalMonthVisible(true)
+  }
+
+  const getDataByMonth = async () => {
+    if (month) {
+      const data = typeCashFilter
+        ? await getAllDataCashByMonthType(month, year, typeCashFilter)
+        : await getAllDataCashByMonth(month, year)
+
+      setAllDataCash(data?.data);
+      setTotalCashFilter(data?.totalCashFilter);
+    } else {
+      const data = typeCashFilter
+        ? await getAllDataCashByType(typeCashFilter)
+        : await getAllDataCash()
+
+      setAllDataCash(data?.data)
+      setTotalCashFilter(data?.totalCashFilter)
+    }
+
+  }
+
+  const handleSetMonthButton = () => {
+    getDataByMonth()
+    setIsModalMonthVisible(false)
+  }
+
+  useEffect(() => {
+    getCurrentYear()
+  }, [])
+
+  useEffect(() => {
+    getDataByMonth()
+  }, [typeCashFilter])
 
   return (
     <ScrollView>
@@ -102,18 +148,26 @@ export default function HomeScreen() {
           <View style={{ backgroundColor: '#dcdcde', borderRadius: 4, padding: 8, marginBottom: 12 }}>
             <Text>Filter bedasarkan:</Text>
 
-            <View style={{paddingTop: 8, display: 'flex', flexDirection: 'row', gap: 20}}>
-              <CheckBox
-                title='Pemasukan'
-                isChecked={isFilterIncome}
-                onPress={() => filtered('income')}
-              />
-              <CheckBox
-                title='Pengeluaran'
-                isChecked={isFilterSpending}
-                onPress={() => filtered('spending')}
-              />
+            <View style={{paddingTop: 8, display: 'flex', flexDirection: 'row', gap: 8, marginBottom: 8}}>
+              <Pressable onPress={() => filtered('income')} style={isFilterIncome ? styles.optionCashRoundedSelected : styles.optionCashRounded}>
+                <Text style={{color: isFilterIncome ? 'white' : ''}}>Pemasukan</Text>
+              </Pressable>
+
+              <Pressable onPress={() => filtered('spending')} style={isFilterSpending ? styles.optionCashRoundedSelected : styles.optionCashRounded}>
+                <Text style={{color: isFilterSpending ? 'white' : ''}}>Pengeluaran</Text>
+              </Pressable>
             </View>
+
+            <Pressable onPress={showModalMonth} style={({ pressed }) => [pressed ? styles.bgSelected : styles.bgBase, styles.optionFilter]}>
+              {({pressed}) => (
+                <>
+                  <TabBarIcon name='time' size={20} color={pressed ? 'white' : ''} />
+                  <Text style={{color: pressed ? 'white' : '', paddingTop: 1}}>
+                    {month ? `${month} / ${year}` : 'Pilih rentang waktu'}
+                  </Text>
+                </>
+              )}
+            </Pressable>
 
             <View>
               <Text style={[styles.textDarkGray, styles.textLarge]}>
@@ -123,6 +177,57 @@ export default function HomeScreen() {
             </View>
           </View>
         : null }
+
+        <Modal isVisible={isModalMonthVisible}>
+          <View style={styles.modalContainer}>
+
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginHorizontal: 'auto', marginBottom: 12 }}>
+              <Pressable onPress={() => setYear(year - 1)}>
+                <TabBarIcon name='chevron-back' />
+              </Pressable>
+              <Text style={{ fontSize: 20, width: 60, textAlign: 'center' }}>{year}</Text>
+              <Pressable onPress={() => setYear(year + 1)}>
+                <TabBarIcon name='chevron-forward' />
+              </Pressable>
+            </View>
+            
+            <View style={styles.monthOptionContainer}>
+              <MonthOption name='Jan' isActive={month === '1' ? true : false} onPress={() => month === '1' ? setMonth('') : setMonth('1')} />
+              <MonthOption name='Feb' isActive={month === '2' ? true : false} onPress={() => month === '2' ? setMonth('') : setMonth('2')} />
+              <MonthOption name='Mar' isActive={month === '3' ? true : false} onPress={() => month === '3' ? setMonth('') : setMonth('3')} />
+            </View>
+
+            <View style={styles.monthOptionContainer}>
+              <MonthOption name='Apr' isActive={month === '4' ? true : false} onPress={() => month === '4' ? setMonth('') : setMonth('4')} />
+              <MonthOption name='Mei' isActive={month === '5' ? true : false} onPress={() => month === '5' ? setMonth('') : setMonth('5')} />
+              <MonthOption name='Jun' isActive={month === '6' ? true : false} onPress={() => month === '6' ? setMonth('') : setMonth('6')} />
+            </View>
+
+            <View style={styles.monthOptionContainer}>
+              <MonthOption name='Jul' isActive={month === '7' ? true : false} onPress={() => month === '7' ? setMonth('') : setMonth('7')} />
+              <MonthOption name='Aug' isActive={month === '8' ? true : false} onPress={() => month === '8' ? setMonth('') : setMonth('8')} />
+              <MonthOption name='Sep' isActive={month === '9' ? true : false} onPress={() => month === '9' ? setMonth('') : setMonth('9')} />
+            </View>
+
+            <View style={styles.monthOptionContainer}>
+              <MonthOption name='Okt' isActive={month === '10' ? true : false} onPress={() => month === '10' ? setMonth('') : setMonth('10')} />
+              <MonthOption name='Nov' isActive={month === '11' ? true : false} onPress={() => month === '11' ? setMonth('') : setMonth('11')} />
+              <MonthOption name='Des' isActive={month === '12' ? true : false} onPress={() => month === '12' ? setMonth('') : setMonth('12')} />
+            </View>
+
+            <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 12}}>
+              <Pressable onPress={() => setIsModalMonthVisible(false)} style={{backgroundColor: '#6b6b6b', borderRadius: 4, paddingVertical: 6, paddingHorizontal: 8}}>
+                <Text style={{color: 'white'}}>Kembali</Text>
+              </Pressable>
+
+              <Pressable onPress={handleSetMonthButton} style={{backgroundColor: '#1bb55b', borderRadius: 4, paddingVertical: 6, paddingHorizontal: 8}}>
+                <Text style={{color: 'white'}}>Sesuaikan</Text>
+              </Pressable>
+
+            </View>
+
+          </View>
+        </Modal>
 
 
         { allDataCash ? allDataCash.map((data: any) => (
@@ -143,6 +248,44 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  optionFilter: {
+    marginVertical: 4,
+    marginEnd: 'auto',
+    borderWidth: 1.5,
+    borderRadius: 4,
+    padding: 8,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 4
+  },
+  bgBase: {
+    borderColor: '#6b6b6b',
+  },
+  bgSelected: {
+    borderColor: '#4678e3',
+    backgroundColor: '#4678e3',
+  },
+  optionCashRounded: {
+    borderColor: '#6b6b6b',
+    borderWidth: 1.5,
+    borderRadius: 4,
+    padding: 6,
+  },
+  optionCashRoundedSelected: {
+    borderColor: '#4678e3',
+    backgroundColor: '#4678e3',
+    borderWidth: 1.5,
+    borderRadius: 4,
+    padding: 6,
+  },
+  monthOptionContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 10,
+    marginHorizontal: 'auto',
+    marginBottom: 10,
+  },
   iconWallet: {
     fontSize: 20,
     paddingRight: 8
